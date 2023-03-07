86b54772075b538806b3683026a17111f456cb43
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-03-07 17:26:22 +0000

    adjusted code to pycodestyle

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+
 def print_last_digit(number):
-    
+
     if number < 0:
         number = number * (-1)
     print("{}".format(number % 10), end="")
+
     return (number % 10)
