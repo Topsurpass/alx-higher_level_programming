db0ad953f49abdc17c58494bff083bf2b255515f
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-03-09 23:29:55 +0000

    adjusted requirement fails

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(len(argv) - 1))
     elif len(argv) == 2:
         print("{:d} argument:".format(len(argv) - 1))
-    elif len(argv) > 2:
+    else:
         print("{:d} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print("{:d}: {:s}".format(i, argv[i]))
+    for i in range(1, len(argv)):
+        print("{:d}: {}".format(i, argv[i]))
