48ff08ea4c09a8f5d7c159ca4211239301390f3e
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-03-13 01:42:09 +0000

    initial commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, alloc, idx;
+
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+	for (idx = 0; idx < size; idx++)
+	{
+		printf("Element %ld: %s\n",
+		       idx,
+		       (PY_TYPE(PyList_GetItem(p, idx)))->tp_name);
+	}
+}
