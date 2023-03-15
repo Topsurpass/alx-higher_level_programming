6eb767856a2acec4fa4942f3523852560e71e0f3
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-03-15 23:27:16 +0000

    testing task 16

--- 0x04-python-more_data_structures/cpython_test.c
+++ 0x04-python-more_data_structures/cpython_test.c
@@ -1,20 +1,16 @@
 #include <Python.h>
-#include <stdio.h>
-#include <string.h>
 
-void print_python_bytes(PyObject *p);
 void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
 
 /**
  * print_python_list - Prints basic info about Python lists.
  * @p: PyObject list object.
  */
-
 void print_python_list(PyObject *p)
 {
 	int size, alloc, i;
 	const char *type;
-
 	PyListObject *list = (PyListObject *)p;
 	PyVarObject *var = (PyVarObject *)p;
 
@@ -32,43 +28,37 @@ void print_python_list(PyObject *p)
 		if (strcmp(type, "bytes") == 0)
 			print_python_bytes(list->ob_item[i]);
 	}
-
 }
 
 /**
  * print_python_bytes - Prints basic info about Python byte objects.
  * @p: PyObject byte object.
  */
-
 void print_python_bytes(PyObject *p)
 {
 	unsigned char i, size;
-	const char *type;
-
-	PyBytesObject *bytes = NULL;
-	PyVarObject *var = (PyVarObject *)p;
+	pyBytesObject *bytes = (PyBytesObject *)p;
 
 	printf("[.] bytes object info\n");
+
 	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-	bytes = (PyBytesObject *)p;
 
-	printf("  size: %ld\n", var->ob_size);
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
 	printf("  trying string: %s\n", bytes->ob_sval);
 
-	if (var->ob_size > 10)
+	if (((PyVarObject *)p)->ob_size > 10)
 		size = 10;
 	else
-		size = var->ob_size + 1;
+		size = ((PyVarObject *)p)->ob_size + 1;
 
 	printf("  first %d bytes: ", size);
-
 	for (i = 0; i < size; i++)
 	{
-		printf("%02hhx", *(bytes->ob_sval + i));
+		printf("%02hhx", bytes->ob_sval[i]);
 		if (i == (size - 1))
 			printf("\n");
 		else
