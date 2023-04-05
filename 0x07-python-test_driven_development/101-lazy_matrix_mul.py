b8df7d15485c3a1a43172042e3fb1f4713f68efe
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-04-05 08:23:34 +0000

    corrected numpy error

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,9 +5,15 @@ Module 101-lazy_matrix-mul
 Ths module contains functions that multiply 2 matrices together
 using the numpy module
 """
-import numpy
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Return matrix multiplied together using numpy"""
-    return numpy.matmul(m_a, m_b)
+    """Return matrix multiplied together using numpy
+    Args:
+        m_a: matrix a
+        m_b: matrix b
+    Returns:
+        result of the multiplication
+    """
+    return np.matmul(m_a, m_b)
