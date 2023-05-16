8d03b3c22bbf205b6913f7313a7d2aaa74ef8306
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-05-16 21:49:30 +0000

    Advance task

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- Import table from temperature.sql in hbtn_0c_0 database 
+-- Displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+
+SELECT city, AVG(value) AS "avg_temp" FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,7 @@
+-- Import in hbtn_0c_0 database the table in temperature.sql.
+-- Displays the max temperature of each state (ordered by State name).
+
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state ASC
