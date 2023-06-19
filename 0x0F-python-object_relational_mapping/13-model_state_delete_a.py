ffb5354827c0489c1526e35d4173f867d22fcc0d
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-06-19 08:57:04 +0000

    adjusted code to commit only after all deletion

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -27,6 +27,6 @@ if __name__ == '__main__':
     states = session.query(State).filter(State.name.like('%a%')).all()
     for state in states:
         session.delete(state)
-        session.commit()
 
+    session.commit()
     session.close()
