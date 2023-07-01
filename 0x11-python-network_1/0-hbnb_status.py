7df813b38ca123c5b403929c58dfcda472335464
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-07-01 16:42:19 +0000

    fix requirement issues

--- 0x11-python-network_1/0-hbnb_status.py
+++ 0x11-python-network_1/0-hbnb_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://alx-intranet.hbtn.io/status
+Script that fetch https://intranet.hbtn.io/status and display response
 """
+
 import urllib.request
 
-
 if __name__ == "__main__":
-    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:")
+        print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
