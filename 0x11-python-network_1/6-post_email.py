ce1f6b520b3fc7eb8a4d66ed72178c545078eaca
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-07-01 01:45:37 +0000

    resolved bugs

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email address, sends a POST request to
-the passed URL with the email as a parameter, and finally displays the body of
-the response.
+Python script that takes in a URL and an email address, sends a POST
+request to the passed URL with the email as a parameter, and finally
+displays the body of the response.
 """
+from sys import argv
+import requests
 
-
-if __name__ = "__main__":
-    import requests
-    from sys import argv
-
-
+if __name__ == "__main__":
+    url = argv[1]
     payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
+    r = requests.post(url, data=payload)
     print(r.text)
