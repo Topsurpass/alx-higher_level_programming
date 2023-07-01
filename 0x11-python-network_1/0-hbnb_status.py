3bb7669e3647246d63070d5cf18b71adddc7e87b
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-07-01 01:51:08 +0000

    re-executed file

--- 0x11-python-network_1/0-hbnb_status.py
+++ 0x11-python-network_1/0-hbnb_status.py
@@ -3,12 +3,13 @@
 Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
-import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        the_head = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(the_head)))
+        print("\t- content: {}".format(the_head))
+        print("\t- utf8 content: {}".format(the_head.decode('utf-8')))
